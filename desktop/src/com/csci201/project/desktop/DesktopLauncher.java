6cc0edc102131418a99bf43058ee2dc16d310f6a
Kevin Kim <kevinsk@usc.edu>
2014-11-19 19:39:45 -0800

    gdx setup

---
+++ desktop/src/com/csci201/project/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.csci201.project.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.csci201.project.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}

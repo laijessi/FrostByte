dcd2b67da64e1614f49793a35d563a05d03c402e
Amos Byon <abyon@usc.edu>
2014-11-21 22:14:30 -0800

    Added more UI elements to Main Menu. Should be fine for now. Still need to add in button click listener to switch it into the game screen.

--- core/src/com/csci201/project/ScreenMainMenu.java
+++ core/src/com/csci201/project/ScreenMainMenu.java
@@ -4,18 +4,31 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class ScreenMainMenu implements Screen{
 	//class variables
 	Game game;
 	Stage stage;
+	Skin skin;
 	
 	private TextButton buttonPlay;
+	private TextField fieldUserName;
 	private TextField fieldHostName;
+	
+	private Label gameNameLabel;
+	private Label usernameLabel;
+	private Label hostnameLabel;
+	private Label statusLabel;
+	
+	private Table table;
 	
 	private int xCen;
 	private int yCen;
@@ -23,31 +36,66 @@ public class ScreenMainMenu implements Screen{
 	//constructor
 	public ScreenMainMenu(Game g){
 		game = g;
-		stage = new Stage();
+		
+		initUIElem();
 		
 		xCen = Gdx.graphics.getWidth()/2;
 		yCen = Gdx.graphics.getHeight()/2;
 		
 		Gdx.input.setInputProcessor(stage);
 		
-		Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+		addUIElem();
+	}
+	
+	public void initUIElem(){
+		stage = new Stage();
+		table = new Table();
+		skin = new Skin(Gdx.files.internal("uiskin.json"));
+		
+		gameNameLabel = new Label("FrostByte", skin);
+		usernameLabel = new Label("Username:", skin);
+		hostnameLabel = new Label("Host:", skin);
+		statusLabel = new Label("", skin);
 		
 		buttonPlay = new TextButton("Play", skin);
-		buttonPlay.setPosition(xCen-150, yCen-30);
-		buttonPlay.setSize(300, 60);
 		
 		fieldHostName = new TextField("", skin);
-		fieldHostName.setPosition(xCen-150, yCen-60);
-		fieldHostName.setSize(300, 30);
+		fieldUserName = new TextField("", skin);
+	}
+	
+	public void addUIElem(){
+		//table.setDebug(true);
 		
-		stage.addActor(buttonPlay);
-		stage.addActor(fieldHostName);
+		table.setPosition(xCen, yCen);
+		table.add(gameNameLabel).colspan(2);
+		table.row();
+		table.add(usernameLabel).pad(10);
+		table.add(fieldUserName);
+		table.row();
+		table.add(hostnameLabel).right().pad(10);
+		table.add(fieldHostName);
+		table.row();
+		table.add(buttonPlay).colspan(2).width(200);
+		table.row();
+		table.add(statusLabel).colspan(2);
+		
+		stage.addActor(table);
+	}
+	
+	public void addListeners(){
+		buttonPlay.addListener(new ClickListener(){
+			@Override
+			public void touchUp(InputEvent e, float x, float y, int point, int button){
+				//TODO: start the game here
+				//game.setScreen(screen);
+			}
+		});
 	}
 
 	@Override
 	public void render(float delta) {
 		// TODO Auto-generated method stub
-		Gdx.gl.glClearColor(1, 1, 1, 0);
+		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		stage.act(delta);
